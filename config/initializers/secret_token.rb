4058cff2e52f63d089d79eea3e1d0d4c7cc5a3b0
Akaruilabs <akaruilabs@Akaruilabs-2.(none)>
2013-07-09 10:43:14 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivityHelper::Application.config.secret_token = '872b08374b66c8bc5c3a5a37e1a6b0b9959db4673a9012e0ce2fbf87c1cc325d21e06c35049ca957b0f887cd7e4f5398a04d245be7d64edc5b791b41eba44f22'
